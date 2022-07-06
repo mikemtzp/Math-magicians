dbbb0c4b7c3b8951e4d53f384abbede26f981f18
jevazquezb <je.vazquezb@gmail.com>
2022-07-06 17:29:59 -0500

    Add interaction test partially

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+// import {create, act} from 'react-test-renderer';
+import calculate from '../logic/calculate';
 import Calculator from './Calculator';
 
 it('Renders Calculator component correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Simulating user interaction', () => {
+  const component = renderer.create(<Calculator />);
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.updateCalc('8');
+  });
+  // re-rendering
+  tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
