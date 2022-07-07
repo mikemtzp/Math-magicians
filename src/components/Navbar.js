a7227d48aec84cfa92707eae34a4c7838f0e64d0
mikemtzp <mikemp8226@gmail.com>
2022-07-07 11:32:42 -0500

    Fix home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import '../styles/NavBar.css';
 const Navbar = () => (
   <nav className="navbar">
     <h1>
-      <Link to="/home" className="logo">
+      <Link to="/" className="logo">
         Math Magicians
       </Link>
     </h1>
     <ul>
       <li>
-        <Link to="/home" id="home-link">Home</Link>
+        <Link to="/" id="home-link">Home</Link>
       </li>
       <li>
         <Link to="/calculator" id="calc-link">Calculator</Link>
