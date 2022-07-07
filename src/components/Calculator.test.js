de6f6aa854bea94fe68eb0fd31088e158edf80ee
jevazquezb <je.vazquezb@gmail.com>
2022-07-07 09:59:53 -0500

    Add navbar and calculator tests

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import {create, act} from 'react-test-renderer';
-import calculate from '../logic/calculate';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from './Calculator';
 
-it('Renders Calculator component correctly', () => {
-  const tree = renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-it('Simulating user interaction', () => {
-  const component = renderer.create(<Calculator />);
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  renderer.act(() => {
-    tree.props.updateCalc('8');
+describe('Calculator tests', () => {
+  it('Renders Calculator component correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+
+  it('Simulation of user interaction with the calculator: 7', () => {
+    render(<Calculator />);
+    const keySeven = screen.getByText('7');
+    const input = document.querySelector('.input');
+    fireEvent.click(keySeven);
+    expect(input.textContent).toBe('7');
+  });
+
+  it('Simulation of user interaction with the calculator: 9.4', () => {
+    render(<Calculator />);
+    const keyNine = screen.getByText('9');
+    const keyDot = screen.getByText('.');
+    const keyFour = screen.getByText('4');
+    const input = document.querySelector('.input');
+    fireEvent.click(keyNine);
+    fireEvent.click(keyDot);
+    fireEvent.click(keyFour);
+    expect(input.textContent).toBe('9.4');
+  });
+
+  it('Simulation of user interaction with the calculator: %', () => {
+    render(<Calculator />);
+    const keyEight = screen.getByText('8');
+    const keyTimes = screen.getByText('x');
+    const keySeven = screen.getByText('7');
+    const keyEqual = screen.getByText('=');
+    const input = document.querySelector('.input');
+    fireEvent.click(keyEight);
+    fireEvent.click(keyTimes);
+    fireEvent.click(keySeven);
+    fireEvent.click(keyEqual);
+    expect(input.textContent).toBe('56');
+  });
 });
--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter as Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import Navbar from './Navbar';
+import App from '../App';
 
-it('Renders Navbar component correctly', () => {
-  const tree = renderer
-    .create(
-      <Router>
-        <Navbar />
+describe('Navbar tests', () => {
+  it('Renders Navbar component correctly', () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <Navbar />
+          {' '}
+        </Router>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Navigate to Home page from Calculator page', () => {
+    render(
+      <Router initialEntries={['/calculator']}>
+        <App />
         {' '}
       </Router>,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+    );
+    const homeLink = document.querySelector('#home-link');
+    fireEvent.click(homeLink);
+    expect(document.body.textContent).toMatch(/welcome/i);
+  });
+
+  it('Navigate to Calculator page from Home page', () => {
+    render(
+      <Router initialEntries={['/home']}>
+        <App />
+        {' '}
+      </Router>,
+    );
+    const calcLink = document.querySelector('#calc-link');
+    fireEvent.click(calcLink);
+    expect(document.body.textContent).toMatch(/some/i);
+  });
+
+  it('Navigate to Quote page from Home page', () => {
+    render(
+      <Router initialEntries={['/home']}>
+        <App />
+        {' '}
+      </Router>,
+    );
+    const quoteLink = document.querySelector('#quote-link');
+    fireEvent.click(quoteLink);
+    expect(document.body.textContent).toMatch(/albert/i);
+  });
 });
